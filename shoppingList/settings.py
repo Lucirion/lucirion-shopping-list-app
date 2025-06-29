b3f692e62da56585639462853c63189581e396b3
Lucirion <lucas_95sjolund@hotmail.com>
2025-06-29 19:56:56 +0900

    Comment out STATICFILES_DIRS to fix static file delivery

--- shoppingList/settings.py
+++ shoppingList/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'shoppinglistApp/static',
-]
+# STATICFILES_DIRS = [
+#    BASE_DIR / 'shoppinglistApp/static',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
